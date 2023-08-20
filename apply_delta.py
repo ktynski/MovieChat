6adf016b73582a719c66164b8eba45eca88b6868
ktynski <kristin@frac.tl>
2023-08-20 15:31:15 -0400

    Update apply_delta.py

--- apply_delta.py
+++ apply_delta.py
@@ -1,25 +1,43 @@
-"""
-Apply the delta weights on top of a base model.
-Adapted from: https://github.com/lm-sys/FastChat/blob/main/fastchat/model/apply_delta.py.
-"""
 import argparse
-
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
+import shutil
+import json
+
+def apply_delta(base_model_identifier, target_model_path, delta_repo_url):
+    # Create a directory for the base model
+    base_model_dir = "/content/llama-7b-hf-v0"
+    
+    # Remove the existing directory if it exists
+    if os.path.exists(base_model_dir):
+        os.system(f"rm -rf {base_model_dir}")
+
+    # Clone the base model repository using Git
+    os.system(f"git clone {base_model_identifier} {base_model_dir}")
 
 
-def apply_delta(base_model_path, target_model_path, delta_path):
-    print(f"Loading the base model from {base_model_path}")
-    base = AutoModelForCausalLM.from_pretrained(
-        base_model_path, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    # Create a directory for the delta model
+    delta_model_dir = "/content/vicuna-7b-delta-v0"
+    
+    # Remove the existing directory if it exists
+    if os.path.exists(delta_model_dir):
+        os.system(f"rm -rf {delta_model_dir}")
 
-    print(f"Loading the delta from {delta_path}")
-    delta = AutoModelForCausalLM.from_pretrained(delta_path, torch_dtype=torch.float16, low_cpu_mem_usage=True)
-    delta_tokenizer = AutoTokenizer.from_pretrained(delta_path, use_fast=False)
+    # Clone the delta repository using Git
+    os.system(f"git clone {delta_repo_url} {delta_model_dir}")
+
+    print(f"Loading the base model from {base_model_dir}")
+    base = AutoModelForCausalLM.from_pretrained(base_model_dir, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+
+
+    print(f"Loading the delta from {delta_model_dir}")
+    delta = AutoModelForCausalLM.from_pretrained(delta_model_dir, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+
 
     DEFAULT_PAD_TOKEN = "[PAD]"
-    base_tokenizer = AutoTokenizer.from_pretrained(base_model_path, use_fast=False)
+    base_tokenizer = AutoTokenizer.from_pretrained(base_model_dir, use_fast=False)
     num_new_tokens = base_tokenizer.add_special_tokens(dict(pad_token=DEFAULT_PAD_TOKEN))
 
     base.resize_token_embeddings(len(base_tokenizer))
@@ -35,15 +53,13 @@ def apply_delta(base_model_path, target_model_path, delta_path):
 
     print(f"Saving the target model to {target_model_path}")
     base.save_pretrained(target_model_path)
-    delta_tokenizer.save_pretrained(target_model_path)
-
+    base_tokenizer.save_pretrained(target_model_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--base-model-path", type=str, required=True)
-    parser.add_argument("--target-model-path", type=str, required=True)
-    parser.add_argument("--delta-path", type=str, required=True)
+    parser.add_argument("--base-model-identifier", type=str, required=True, help="Identifier for the base model on Hugging Face (e.g., 'decapoda-research/llama-13b-hf')")
+    parser.add_argument("--target-model-path", type=str, required=True, help="Path to save the target model")
+    parser.add_argument("--delta-repo-url", type=str, required=True, help="URL of the delta model repository on Hugging Face (e.g., 'https://huggingface.co/lmsys/vicuna-7b-delta-v0')")
 
     args = parser.parse_args()
-
-    apply_delta(args.base_model_path, args.target_model_path, args.delta_path)
+    apply_delta(args.base_model_identifier, args.target_model_path, args.delta_repo_url)
